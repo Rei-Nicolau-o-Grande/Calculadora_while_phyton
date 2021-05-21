fa2529f3c03df67cdfae1fabeffdac3671acea68
McLovin <63524589+This-is-called-strategy@users.noreply.github.com>
2021-05-21 16:48:48 -0300

    Update calculadora while.py

--- calculadora while.py
+++ calculadora while.py
@@ -3,12 +3,12 @@ while True:
     num2 = input("Digite outro numero: ")
     operador = input("Escolha o operador (ex: +, -, *, /): ")
 
-    if  not num1.isnumeric() or not num2.isnumeric():
+    if  not num1.isnumeric() or not num2.isnumeric(): # Verificar quando o usuario vai digitar um numero
         print("Erro Digite um numero.")
         continue
 
-    num1 = int(num1)
-    num2 = int(num2)
+    num1 = int(num1) # transformando a variavel de boolean para inteiro
+    num2 = int(num2) # # transformando a variavel de boolean para inteiro
 
     if operador == "+":
         print(f"Resultado: {num1 + num2}")
